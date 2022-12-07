3dab2c6c030f28a3a996d9b274b7b3f59f0fb153
Purrie <purriestarshine@gmail.com>
2022-12-07 19:43:01 +0100

    testing doom emacs local packages

---
+++ lsl-mode.el
@@ -0,0 +1,28 @@
+;;; lsl-mode.el --- Major mode for Linden Scripting Language -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Purrie
+;;
+;; Author: Purrie <purriestarshine@gmail.com>
+;; Maintainer: Purrie <purriestarshine@gmail.com>
+;; Created: December 07, 2022
+;; Modified: December 07, 2022
+;; Version: 0.0.1
+;; Keywords: languages tools
+;; Homepage: https://github.com/purrie/lsl-mode
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Major mode for Linden Scripting Language (LSL)
+;;
+;;; Code:
+
+(defun lsl-mode-do-a-thing ()
+  "This does a thing."
+  (interactive)
+  (message "I did a thing"))
+
+(provide 'lsl-mode)
+;;; lsl-mode.el ends here
